ab2bff1d843af2b4377453452a5f327e1c073605
Juan Augusto Gomes Vieira <jg14253@gmail.com>
2024-07-26 16:03:51 -0300

    retirando types, moves e abilities

--- src/main/java/bradesco/banco/PokeApi/model/Pokemon.java
+++ src/main/java/bradesco/banco/PokeApi/model/Pokemon.java
@@ -2,17 +2,24 @@ package bradesco.banco.PokeApi.model;
 
 import java.util.List;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Pokemon {
+    @Id
     private Long id;
     private String name;
-    private List<Type> types;
-    private List<Move> moves;
-    private List<Ability> abilities;
+
+    private int height;
+    private int weight;
+
+    @ManyToMany(mappedBy = "pokemons")
+    private List<Pokedex> pokedexes;
 
 }
